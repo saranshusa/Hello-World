605b0d940eaea0d9c58d98d1ebfd59932a8346bd
Saransh Gupta <82395600+saranshusa@users.noreply.github.com>
2021-05-06 23:33:12 +0530

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+# HelloWorld.py
+# This is my first python program which prints "Hello World!".
+# It is an old tradition, so I am also happy to be a part of it.
+
+def main():
+    print('Hello World!')
+
+if __name__ == '__main__':
+    main()
+
+# That's it !
